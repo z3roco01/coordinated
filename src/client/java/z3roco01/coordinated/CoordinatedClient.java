e1afe356a8ee9c661e6530e228e36cd45aa33de6
z3roco01 <zerocool@e.email>
2026-01-07 08:51:47 -0600

    init commit :p

---
+++ src/client/java/z3roco01/coordinated/CoordinatedClient.java
@@ -0,0 +1,10 @@
+package z3roco01.coordinated;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoordinatedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/z3roco01/coordinated/Coordinated.java
@@ -0,0 +1,15 @@
+package z3roco01.coordinated;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Coordinated implements ModInitializer {
+	public static final String MOD_ID = "coordinated";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
